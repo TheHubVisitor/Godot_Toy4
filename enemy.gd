f5202444f920fd8e0f3f9212e9cfb57eafb7da4e
bestadamwarlockmain <kekoa.enomoto@imua.ksbe.edu>
2025-03-16 10:55:59 -1000

    worked on spawner, enemy
    why does it not spawn the damn things

--- enemy.gd
+++ enemy.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var navigation_agent : NavigationAgent2D = $NavigationAgent2D
+@export var chase_target : CharacterBody2D
+var speed = 180
+var accel = 7
 
 
 func _physics_process(delta: float) -> void:
-
-
+	var direction = Vector3()
+	navigation_agent.target_position = chase_target.global_position
+	direction = navigation_agent.get_next_path_position() - global_position
+	direction = direction.normalized()
+	velocity = velocity.lerp(direction * speed , accel * delta)
+	
 	move_and_slide()
